7c4a36f84fdd80b0cddfb60dd4109eb91c76ae0f
emuhlestein <emuhlestein@yahoo.com>
2020-10-17 14:13:00 -0500

    Initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
